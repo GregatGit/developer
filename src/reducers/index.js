3f2fd960e354d713cc94b92b6515c61e566efc61
gregatgit <gregdd@outlook.com>
2020-07-02 13:49:16 +1000

    first commit

---
+++ src/reducers/index.js
@@ -0,0 +1,8 @@
+import { combineReducers } from 'redux'
+
+import temp from './reducer_temp'
+
+
+export default combineReducers({
+  temp
+})
---
+++ src/reducers/reducer_temp.js
@@ -0,0 +1,8 @@
+const nameInitialState = {data: {
+  one: 'hi'
+}}
+const temp = (state = nameInitialState, action) => {
+  return state
+}
+
+export default temp
