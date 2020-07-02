2ebb2eb1a1c8c82aa82e38e9a206ef1aaa765a21
gregatgit <gregdd@outlook.com>
2020-07-02 16:54:05 +1000

    small updates

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import React from 'react';
+import SidePanel from './components/SidePanel';
 
 function App() {
   return (
-    <div>
-      Developer
+    <div id="main">
+      <SidePanel />
     </div>
   )
 }
