27fe97ab46bffda5c4a504e0031f60bf411d25a4
gregatgit <gregdd@outlook.com>
2020-07-07 15:34:36 +1000

    make elements fade in

--- src/components/Pages/Welcome.jsx
+++ src/components/Pages/Welcome.jsx
@@ -1,24 +1,59 @@
-import React from 'react'
-import {ReactComponent as ReactLogo} from '../../img/logo-react.svg'
-import {ReactComponent as ReduxLogo} from '../../img/logo-redux.svg'
-import {ReactComponent as JSLogo} from '../../img/logo-javascript.svg'
-import {ReactComponent as CssLogo} from '../../img/logo-css.svg'
-import {ReactComponent as FirebaseLogo} from '../../img/logo-firebase.svg'
-
+import React, { Fragment } from 'react'
+import { Grow } from '@material-ui/core'
+import { ReactComponent as ReactLogo } from '../../img/logo-react.svg'
+import { ReactComponent as ReduxLogo } from '../../img/logo-redux.svg'
+import { ReactComponent as JSLogo } from '../../img/logo-javascript.svg'
+import { ReactComponent as CssLogo } from '../../img/logo-css.svg'
+import { ReactComponent as FirebaseLogo } from '../../img/logo-firebase.svg'
 
 const Welcome = () => {
+  const TIME = 1500
   return (
     <div className="welcome">
-      <h2>Welcome</h2>
-      <p>I am Greg Duncan, a web developer building apps with modern technologies. My main focus has been utilising React with Redux with Firebase as a back end but I have used Vue and Vanilla.js to build apps. </p>
-    <div className="welcomeLogo">
-      <span><JSLogo /></span>
-      <span><ReactLogo width="100" height="100"/></span>
-      <span><FirebaseLogo width="100" height="100"/></span>
-      <span><ReduxLogo width="100" height="100"/></span>
-      <span><CssLogo width="100" height="100"/></span>
-    </div>
-
+      <Fragment>
+        <Grow
+          in={true}
+          style={{ transformOrigin: '0 0 0' }}
+          {...(true ? { timeout: TIME } : {})}
+        >
+          <h2>Welcome</h2>
+        </Grow>
+        <Grow
+          in={true}
+          style={{ transformOrigin: '0 0 0' }}
+          {...(true ? { timeout: TIME + 1000 } : {})}
+        >
+          <p>
+            I am Greg Duncan, a web developer building apps with modern
+            technologies. My main focus has been utilising React with Redux. Materail-UI and CSS for styling and
+            Firebase as a back end. I also use Vue and Vanilla.js to build
+            apps.
+          </p>
+        </Grow>
+        <Grow
+          in={true}
+          style={{ transformOrigin: '0 0 0' }}
+          {...(true ? { timeout: TIME + 2500 } : {})}
+        >
+          <div className="welcomeLogo">
+            <span>
+              <JSLogo />
+            </span>
+            <span>
+              <ReactLogo />
+            </span>
+            <span>
+              <FirebaseLogo />
+            </span>
+            <span>
+              <ReduxLogo />
+            </span>
+            <span>
+              <CssLogo />
+            </span>
+          </div>
+        </Grow>
+      </Fragment>
     </div>
   )
 }
