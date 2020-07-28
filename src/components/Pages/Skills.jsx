2fac7a48d781226ed1d217fd685f7de67a0437ff
gregatgit <gregdd@outlook.com>
2020-07-28 13:06:02 +1000

    update skills

--- src/components/Pages/Skills.jsx
+++ src/components/Pages/Skills.jsx
@@ -1,9 +1,77 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { makeStyles } from '@material-ui/core/styles'
+import { Grid, Paper, Grow } from '@material-ui/core/'
+import FlashingIcon from './PageComponets/FlashingIcon'
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    flexGrow: 1,
+    position: 'absolute',
+
+  },
+  paper: {
+    height: 140,
+    width: 100,
+  },
+  control: {
+    padding: theme.spacing(2),
+  },
+}))
 
 const Skills = () => {
+
+  const classes = useStyles()
+  const [show, setShow] = useState(true)
+
+  const TIME = 1400
+  
   return (
-    <div>
-      Skills page
+    <div className="skills container">
+    <Grow
+    in={show}
+    style={{ transformOrigin: '0 0 0' }}
+    {...(show ? { timeout: TIME } : {})}
+  >
+      <h2>Skills</h2>
+      </Grow>
+      <Grow
+        in={show}
+        style={{ transformOrigin: '0 0 0' }}
+        {...(show ? { timeout: TIME + 400 } : {})}
+      >
+      <article>
+        <p>
+          I build projects with React, Redux, Material-Ui and if a back
+          end is needed Firebase. In the past I configured Webpack but now I
+          take advantage of Create-React-App.
+        </p>
+        <p>
+          I have also built apps with Vue, Vanilla, jQuerry with Bootstrap and
+          Semantic-Ui.
+        </p>
+        <p>I have also coded with C, C#, MongoDB and Knockout.</p>
+        <p>
+          I have a Diploma in Networking (completed 2014) that grounded me in
+          the network fundamentals and taught me how to configure Cisco routers
+          and manage Windows Server and Active Directory.
+        </p>
+        <p>
+          Outside the digital world I host Murder Mysteries and Trivia nights which has made me a
+          confident public speaker.
+        </p>
+      </article>
+      </Grow>
+      <Grid container className={classes.root} spacing={2}>
+        <Grid item xs={12}>
+          <Grid container justify="center" spacing={2}>
+            {[0, 1, 2, 3, 4].map((logo, index) => (
+              <Grid key={index} item className="logo1">
+                <FlashingIcon wait={logo} />
+              </Grid>
+            ))}
+          </Grid>
+        </Grid>
+      </Grid>
     </div>
   )
 }
