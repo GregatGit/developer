063ecc06eafba6fc11c1c126653b655710d2c0d1
gregatgit <gregdd@outlook.com>
2020-08-02 19:11:36 +1000

    Updates

---
+++ precache-manifest.891367f5e89c2bbdf69ad3118ab264d4.js
@@ -0,0 +1,142 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a2a876ce204a7fdbeeaf965747eb6885",
+    "url": "/developer/index.html"
+  },
+  {
+    "revision": "d754cb7aec4a8c40c389",
+    "url": "/developer/static/css/main.d48de613.chunk.css"
+  },
+  {
+    "revision": "7e162d3d3764864bd731",
+    "url": "/developer/static/js/2.fd66eba6.chunk.js"
+  },
+  {
+    "revision": "5b8b49146ea7ff4abb4f22a9d28772fe",
+    "url": "/developer/static/js/2.fd66eba6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d754cb7aec4a8c40c389",
+    "url": "/developer/static/js/main.66611c38.chunk.js"
+  },
+  {
+    "revision": "5d3fc1b8b1dbeb9fa8aa",
+    "url": "/developer/static/js/runtime-main.26b10b5a.js"
+  },
+  {
+    "revision": "3b300187e123f56ca2e1c5fc6a748ae9",
+    "url": "/developer/static/media/GameOfLife.3b300187.JPG"
+  },
+  {
+    "revision": "feace2c876e8b4ca66a532bdeca98afb",
+    "url": "/developer/static/media/Sunny1.feace2c8.jpg"
+  },
+  {
+    "revision": "e83164e65a03d24ee108161d33e96668",
+    "url": "/developer/static/media/calculator.e83164e6.JPG"
+  },
+  {
+    "revision": "646a1fb64ef42a265caa130318a5a7a9",
+    "url": "/developer/static/media/drum-machine.646a1fb6.JPG"
+  },
+  {
+    "revision": "2618398aaa647ac9c55f29101bb79e47",
+    "url": "/developer/static/media/goShop.2618398a.JPG"
+  },
+  {
+    "revision": "bdadccafe6b454891b4cb71cd9b200a8",
+    "url": "/developer/static/media/greg.bdadccaf.jpg"
+  },
+  {
+    "revision": "4769959fc58b667575960cd67adba25b",
+    "url": "/developer/static/media/iron-throne.4769959f.jpg"
+  },
+  {
+    "revision": "371c920ba10a46409479a49bd9cfc571",
+    "url": "/developer/static/media/logo-css.371c920b.svg"
+  },
+  {
+    "revision": "9e82f6f3096f3e3a39dbdad6f263ae56",
+    "url": "/developer/static/media/logo-firebase.9e82f6f3.svg"
+  },
+  {
+    "revision": "8e4cd62eebdaded0e04cfc7c0be1db2c",
+    "url": "/developer/static/media/logo-git.8e4cd62e.svg"
+  },
+  {
+    "revision": "23e2d9ce53c624499f8ad71bd24367cb",
+    "url": "/developer/static/media/logo-html.23e2d9ce.svg"
+  },
+  {
+    "revision": "3f9ca62fc3b9eb791c709f59c0648d07",
+    "url": "/developer/static/media/logo-javascript.3f9ca62f.svg"
+  },
+  {
+    "revision": "d273971c2e1ca5a41e63f61104a70bdc",
+    "url": "/developer/static/media/logo-jquery.d273971c.svg"
+  },
+  {
+    "revision": "45b9af47b3ed2e467a0053be0d134339",
+    "url": "/developer/static/media/logo-material-ui.45b9af47.svg"
+  },
+  {
+    "revision": "a1f7b38fecbcef723829df8d9bb14fb6",
+    "url": "/developer/static/media/logo-node.a1f7b38f.svg"
+  },
+  {
+    "revision": "84287d09b8053c6fa598893b8910786a",
+    "url": "/developer/static/media/logo-react.84287d09.svg"
+  },
+  {
+    "revision": "ed5f9644ff41004b2d22af06c56c5952",
+    "url": "/developer/static/media/logo-redux.ed5f9644.svg"
+  },
+  {
+    "revision": "00841985016a5d1f818b2b8397ce210e",
+    "url": "/developer/static/media/logo-vscode.00841985.svg"
+  },
+  {
+    "revision": "c937b25cda6aab270a2525f6fa350e85",
+    "url": "/developer/static/media/markdown-previewer.c937b25c.JPG"
+  },
+  {
+    "revision": "3b61f4cd3519d719e711650d2bfd8c55",
+    "url": "/developer/static/media/pomodoro.3b61f4cd.JPG"
+  },
+  {
+    "revision": "70a48e403f0acc291bc5662dcfdb4eeb",
+    "url": "/developer/static/media/projects.70a48e40.JPG"
+  },
+  {
+    "revision": "2434a9c47343a9f6bd6a7c7ce84b67af",
+    "url": "/developer/static/media/quote.2434a9c4.JPG"
+  },
+  {
+    "revision": "0705e9dbe4002551358ba588fda0d4e3",
+    "url": "/developer/static/media/recipebox.0705e9db.JPG"
+  },
+  {
+    "revision": "af9c12c3f2453c46f93430f8fbeddefe",
+    "url": "/developer/static/media/simon-game.af9c12c3.JPG"
+  },
+  {
+    "revision": "7e92b105b09f1418a4862a5deb517802",
+    "url": "/developer/static/media/starryNight.7e92b105.gif"
+  },
+  {
+    "revision": "c4206fb79db049a856119648e70a5353",
+    "url": "/developer/static/media/tictactoe.c4206fb7.JPG"
+  },
+  {
+    "revision": "c8c5e00d22d3fcac9033bb20f65f099d",
+    "url": "/developer/static/media/todoLab.c8c5e00d.JPG"
+  },
+  {
+    "revision": "c5ef9b069b433a42d292ec1aa8844c44",
+    "url": "/developer/static/media/vue-todos.c5ef9b06.JPG"
+  },
+  {
+    "revision": "e410d5bf4a754551651455795194f9f5",
+    "url": "/developer/static/media/wiki.e410d5bf.JPG"
+  }
+]);
