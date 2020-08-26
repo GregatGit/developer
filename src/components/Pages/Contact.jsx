61e7347f266f6912016034586dac42bdaf3e160b
gregatgit <gregdd@outlook.com>
2020-08-26 15:13:45 +1000

    add header

--- src/components/Pages/Contact.jsx
+++ src/components/Pages/Contact.jsx
@@ -1,10 +1,11 @@
-import React, { useState } from 'react'
+import React, { useState, Fragment } from 'react'
 import { CopyToClipboard } from 'react-copy-to-clipboard'
 import { makeStyles } from '@material-ui/core/styles'
 import {
   List,
   ListItem,
   ListItemText,
+  ListSubheader,
   ListItemAvatar,
   Avatar,
   Grow,
@@ -23,6 +24,16 @@ const useStyles = makeStyles((theme) => ({
     // paddingTop: '10%',
     //color: '#fff',
   },
+  listItem: {
+    '&:hover': {
+      cursor: 'pointer',
+    },
+  },
+  header: {
+    color: '#fff',
+    fontSize: '3rem',
+    paddingBottom: '10%'
+  }
 }))
 
 export default function Contacts() {
@@ -32,91 +43,105 @@ export default function Contacts() {
 
   return (
     <div className="contacts-page">
-    <List>
-      <Grow
-        in={show}
-        style={{ transformOrigin: '0 0 0' }}
-        {...(show ? { timeout: TIME } : {})}
+      <List
+        component="nav"
+        aria-labelledby="nested-list-subheader"
+        subheader={
+          <ListSubheader component="div" id="nested-list-subheader">
+            <Grow
+              in={show}
+              style={{ transformOrigin: '0 0 0' }}
+              {...(show ? { timeout: TIME } : {})}
+            >
+              <h2 className={classes.header}>Contact</h2>
+            </Grow>
+          </ListSubheader>
+        }
       >
-        <ListItem>
-          <ListItemAvatar>
-            <Avatar className={classes.avatar}>
-              <MailIcon />
-            </Avatar>
-          </ListItemAvatar>
-          <ListItemText>
-            <a href="mailto:gregdd@outlook.com?subject=contact_from_website&body=Hello">
-              gregdd@outlook.com
-            </a>
-          </ListItemText>
-        </ListItem>
-      </Grow>
-      <Grow
-        in={show}
-        style={{ transformOrigin: '0 0 0' }}
-        {...(show ? { timeout: TIME + 300 } : {})}
-      >
-      <ListItem>
-        <ListItemAvatar>
-          <Avatar>
-            <PhoneIcon />
-          </Avatar>
-        </ListItemAvatar>
-        <ListItemText>
-          <CopyToClipboard text="0422 347 204">
-            <span>0422 347 204</span>
-          </CopyToClipboard>
-        </ListItemText>
-      </ListItem>
-      </Grow>
-      <Grow
-        in={show}
-        style={{ transformOrigin: '0 0 0' }}
-        {...(show ? { timeout: TIME + 600 } : {})}
-      >
-      <ListItem>
-        <ListItemAvatar>
-          <Avatar>
-            <GitHubIcon />
-          </Avatar>
-        </ListItemAvatar>
-        <ListItemText>
-          <a
-            onClick={() =>
-              window.open('https://github.com/GregatGit/', '_blank')
-            }
-          >
-            Open my github page
-          </a>
-        </ListItemText>
-      </ListItem>
-      </Grow>
-      <Grow
-        in={show}
-        style={{ transformOrigin: '0 0 0' }}
-        {...(show ? { timeout: TIME + 900 } : {})}
-      >
-      <ListItem>
-        <ListItemAvatar>
-          <Avatar>
-            <LinkedInIcon />
-          </Avatar>
-        </ListItemAvatar>
-        <ListItemText>
-          <a
-            onClick={() =>
-              window.open(
-                'https://www.linkedin.com/in/greg-duncan-458199108/',
-                '_blank'
-              )
-            }
-          >
-            Open my linkedin page
-          </a>
-        </ListItemText>
-      </ListItem>
-      </Grow>
-    </List>
+        <Grow
+          in={show}
+          style={{ transformOrigin: '0 0 0' }}
+          {...(show ? { timeout: TIME } : {})}
+        >
+          <ListItem>
+            <ListItemAvatar>
+              <Avatar className={classes.avatar}>
+                <MailIcon />
+              </Avatar>
+            </ListItemAvatar>
+            <ListItemText>
+              <a href="mailto:gregdd@outlook.com?subject=contact_from_website&body=Hello">
+                gregdd@outlook.com
+              </a>
+            </ListItemText>
+          </ListItem>
+        </Grow>
+        <Grow
+          in={show}
+          style={{ transformOrigin: '0 0 0' }}
+          {...(show ? { timeout: TIME + 300 } : {})}
+        >
+          <ListItem>
+            <ListItemAvatar>
+              <Avatar>
+                <PhoneIcon />
+              </Avatar>
+            </ListItemAvatar>
+            <ListItemText>
+              <CopyToClipboard text="0422 347 204">
+                <span>0422 347 204</span>
+              </CopyToClipboard>
+            </ListItemText>
+          </ListItem>
+        </Grow>
+        <Grow
+          in={show}
+          style={{ transformOrigin: '0 0 0' }}
+          {...(show ? { timeout: TIME + 600 } : {})}
+        >
+          <ListItem>
+            <ListItemAvatar>
+              <Avatar>
+                <GitHubIcon />
+              </Avatar>
+            </ListItemAvatar>
+            <ListItemText className={classes.listItem}>
+              <a
+                onClick={() =>
+                  window.open('https://github.com/GregatGit/', '_blank')
+                }
+              >
+                Open my github page
+              </a>
+            </ListItemText>
+          </ListItem>
+        </Grow>
+        <Grow
+          in={show}
+          style={{ transformOrigin: '0 0 0' }}
+          {...(show ? { timeout: TIME + 900 } : {})}
+        >
+          <ListItem>
+            <ListItemAvatar>
+              <Avatar>
+                <LinkedInIcon />
+              </Avatar>
+            </ListItemAvatar>
+            <ListItemText className={classes.listItem}>
+              <a
+                onClick={() =>
+                  window.open(
+                    'https://www.linkedin.com/in/greg-duncan-458199108/',
+                    '_blank'
+                  )
+                }
+              >
+                Open my linkedin page
+              </a>
+            </ListItemText>
+          </ListItem>
+        </Grow>
+      </List>
     </div>
   )
 }
